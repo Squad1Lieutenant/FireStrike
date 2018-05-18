9239771c0b5213aa532179479f334fd3960346a5
Jack P <jpirone@wppd.trade>
2018-05-17 20:15:17 -0700

    Add files via upload

---
+++ FireStrikeGame/src/Game/Game.java
@@ -0,0 +1,44 @@
+package Game;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class Game implements ActionListener, KeyListener {
+private static JFrame gameFrame;
+
+	public static void main(String[] Args) {
+		new Game();
+	}
+	
+	public static void game() {
+		gameFrame = new JFrame();
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
