801afe69552a2bdf2bf9f40a10dcf15ced30f622
Jack P <jpirone@wppd.trade>
2018-05-17 21:03:06 -0700

    Add files via upload

--- FireStrikeGame/src/Game/bullet.java
+++ FireStrikeGame/src/Game/bullet.java
@@ -2,14 +2,6 @@ package Game;
 
 import javax.swing.JLabel;
 
-//This interface should be implemented by every class that represents a
-//type of Alien; each of those classes will need to implement all of the
-//methods below; the benefit of having all of the Alien classes under
-//this Alien interface is that they all can be grouped within a single
-//List (e.g., ArrayList) and referenced via that list, instead of
-//needing to be put into separate lists, requiring additional code;
-//using this interface also allows others to know what methods they can
-//expect to find in each of the Alien classes
 public interface bullet
 {
 	public String getType();
