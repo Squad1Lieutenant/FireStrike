d02faaded67f2564c04a3efbef88838ab7ab1087
Jack P <jpirone@wppd.trade>
2018-05-21 12:09:27 -0700

    Update Server.java

--- FireStrikeGame/src/Game/Server.java
+++ FireStrikeGame/src/Game/Server.java
@@ -3,7 +3,8 @@ package Game;
 import java.io.*;
 import java.net.*;
 
-public class Server {
+public class Server
+{
 
 	private static final int MAX_USERS = 10;
 	static ServerSocket serverSocket;
@@ -12,13 +13,16 @@ public class Server {
 	static DataInputStream in;
 	static Users[] user = new Users[MAX_USERS];
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception
+	{
 		System.out.println("Starting Server...");
 		serverSocket = new ServerSocket(7777);
 		System.out.println("Server Started...");
-		while (true) {
+		while (true)
+		{
 			socket = serverSocket.accept();
-			for (int i = 0; i < 10; i++) {
+			for (int i = 0; i < 10; i++)
+			{
 				System.out.println("Connection From:" + socket.getInetAddress());
 				out = new DataOutputStream(socket.getOutputStream());
 				in = new DataInputStream(socket.getInputStream());
@@ -32,30 +36,39 @@ public class Server {
 	}
 }
 
-class Users implements Runnable {
-	private static final int MAX_USERS = 10;
+class Users implements Runnable
+{
 	DataOutputStream out;
 	DataInputStream in;
-	Users[] user = new Users[MAX_USERS];
+	Users[] user = new Users[10];
 
-	public Users(DataOutputStream out, DataInputStream in, Users[] user) {
+	public Users(DataOutputStream out, DataInputStream in, Users[] user)
+	{
 
 	}
 
-	public void run() {
-		while (true) {
-			try {
-				String message = in.readUTF();
-				for (int i = 0; i < MAX_USERS; i++) {
-					if (user[i] != null){
+	public void run()
+	{
+		while (true)
+		{ 
+			String message;
+			try
+			{
+				message = in.readUTF();
+				for (int i = 0; i < 10; i++)
+				{
+					if (user[i] != null)
+					{
 						user[i].out.writeUTF(message);
 					}
 				}
-			} catch (IOException e) {
+			}
+			catch (IOException e)
+			{
 				e.printStackTrace();
 				this.out = null;
 				this.in = null;
-				
+
 			}
 		}
 
