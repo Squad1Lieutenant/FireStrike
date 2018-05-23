90b25a47a583161bed1d5bfa55d26338c4f997e1
Jack P <jpirone@wppd.trade>
2018-05-23 09:17:13 -0700

    Changes
    A chat client that only works if ran in the IDE, due to the need of a console

--- FireStrikeGame/src/Game/Client.java
+++ FireStrikeGame/src/Game/Client.java
@@ -2,20 +2,50 @@ package Game;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
-public class Client
-{
+public class Client {
 	static Socket socket;
 	static DataInputStream in;
+	static DataOutputStream out;
 
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		System.out.println("Connecting");
 		socket = new Socket("localhost", 7777);
 		System.out.println("Connection Established");
 		in = new DataInputStream(socket.getInputStream());
-		System.out.println("...");
-		String test = in.readUTF();
-		System.out.println("Message:" + test);
+		out = new DataOutputStream(socket.getOutputStream());
+		Input input = new Input(in);
+		Thread thread = new Thread(input);
+		thread.start();
+		Scanner sc = new Scanner(System.in);
+		while(true)
+		{
+			String SendMessage = sc.nextLine();
+			out.writeUTF(SendMessage);
+		}
 	}
+}
+
+class Input implements Runnable {
+	DataInputStream in;
+
+	public Input(DataInputStream in) {
+		this.in = in;
+
+	}
+
+	public void run() {
+		while (true) {
+			String message;
+			try {
+				message = in.readUTF();
+				System.out.println(message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
 }
